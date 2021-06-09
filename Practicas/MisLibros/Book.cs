2b7ec0ff47c995d24b576e121f4a099fa502bb17
Diego Pardo <dlplenin@hotmail.com>
2021-06-09 11:24:21 -0500

    Tests TDD

---
+++ Practicas/MisLibros/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practicas.MisLibros
+{
+    public class Book
+    {
+        private readonly string bookIsbn;
+
+        public Book(string bookIsbn)
+        {
+            this.bookIsbn = bookIsbn;
+        }
+    }
+}
---
+++ Practicas/MisLibros/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practicas.MisLibros
+{
+    public class Cart
+    {
+        private List<Book> Items { get; set; } = new List<Book>();
+
+        public void AddBook(Book libro)
+        {
+            Items.Add(libro);
+        }
+
+        public int GetItemsCount()
+        {
+            return Items.Count;
+        }
+    }
+}
